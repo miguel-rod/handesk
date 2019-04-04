afc4228fdf0b63d83c247650507270a6747562d5
miguel-rod <mike.rodriguez91@gmail.com>
2019-04-04 09:45:00 -0500

    meta

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="google-site-verification" content="jzn6ih1CbuVKSlornCh7TZEUY95nXd9vfFAUurwqzuQ" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
